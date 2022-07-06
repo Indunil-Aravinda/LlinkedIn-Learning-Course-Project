24432b0182ce56a38d0cc982323737a1ae0d2dc5
Indunil Aravinda (Creative Software) <Indunil.A@CreativeSoftware.com>
2022-07-06 15:41:35 +0530

    Initial commit

---
+++ src/test/java/com/indunil/linkedinproject/LinkedinProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indunil.linkedinproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
